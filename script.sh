b3c1609f5f7efb867c29ee5e2989d7874b6d7906
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-08-25 11:57:00 +0530

    Automatically update the tg userbot
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

---
+++ script.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/marshmello61/PaperplaneX.git
+cd PaperplaneX
+git remote add upstream https://github.com/AvinashReddy3108/PaperplaneExtended.git
+git pull upstream sql-extended
+git push
