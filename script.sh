e272ace11cf6c0f206f2a433537744ac6336db12
marshmello61 <ultramayur123@gmail.com>
2019-08-25 08:41:19 +0000

    improve the script to nuke errors
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
+rm -rf PaperplaneX
 git clone https://github.com/marshmello61/PaperplaneX.git
 cd PaperplaneX
 git remote add upstream https://github.com/AvinashReddy3108/PaperplaneExtended.git
 git pull upstream sql-extended
 git push
+cd ..
+rm -rf PaperplaneX
