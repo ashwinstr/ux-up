18ade2686327b7d66ee35016b49586a8d643d74e
Mayur Varde <ultramayur123@gmail.com>
2020-06-05 09:16:22 +0530

    Update script for proper y/n
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- script.sh
+++ script.sh
@@ -4,24 +4,29 @@ repo="UserBot"
 rm -rf ${repo}
 
 echo "You're running the UserBot Updater script by @marshmello61"
+echo " "
 # Get username of user
-echo -n "Enter your GitHub username:"
+echo -n "Enter your GitHub username: "
 read userName
 echo " "
 
 # Check if user has forked or has the same repo name
-echo -n "Have you forked UserBot from marshmello61 or has the same repo name? [y/n]: "
+echo -n "Have you forked UserBot from marshmello61 or has the same repo name i.e. UserBot? [y/n]: "
 read fork
 echo " "
 
-if [[ "$fork" == 'y' ]]; then
+if [[ "${fork}" == 'y' ]]; then
 	git clone https://github.com/${userName}/${repo}.git
 	cd ${repo}
-else
+elif [[ "${fork}" == 'n' ]]; then
 	echo -n "Enter your repo name: "
 	read repo
 	git clone https://github.com/${userName}/${repo}.git
 	cd ${repo}
+else
+        echo " "
+        echo "You were only supposed to enter y or n"
+        exit 1
 fi
 
 echo " "
