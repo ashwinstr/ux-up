3e305e2331c0f751559fc0225da79373daad3713
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-27 08:38:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,7 +34,8 @@ fi
 echo " "
 echo "Updating your oub-remix"
 git pull https://github.com/sahyam2019/oub-remix.git
-git commit -m "😎 to"
+git diff
+git commit -m "😎"
 git push
 echo " "
 echo "Updated"
