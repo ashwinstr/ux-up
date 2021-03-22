111302182e8b95da7956d5080d78d5c983b14158
Itachi <70562371+ashwinsutar1996@users.noreply.github.com>
2021-03-22 17:20:36 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,45 +2,24 @@
 
 cd ..
 
-echo -n "Termux or Windows (t/w): "
-read platform
-
 echo " "
 echo """###You're running the USERGE-X update script.###"""
 echo " "
 
 #get username of user
-echo -n "Main or upstream and Kakashi or Itachi (k-m)/(k-u)/(i-m)/(i-u) : "
+echo -n "Kakashi or Itachi or difference (k)/(i)/(d): "
 read u_n
 
-if [[ "${u_n}" == "k-m" ]]; then
-	rm -rf USERGE-X
-	git clone https://github.com/ashwinsutar1996/USERGE-X.git
-	cd USERGE-X
-	git pull
-	git pull https://github.com/ashwinstr/UX-kakashi.git
-	repo="USERGE-X"
-fi
-
-if [[ "${u_n}" == "k-u" ]]; then
+if [[ "${u_n}" == "k" ]]; then
 	rm -rf UX-kakashi
 	git clone https://github.com/ashwinstr/UX-kakashi.git
 	cd UX-kakashi
 	git pull
-	git pull https://github.com/code-rgb/USERGE-X.git
+	git pull https://github.com/ashwinstr/UX-itachi.git
 	repo="UX-kakashi"
 fi
 
-if [[ "${u_n}" == "i-m" ]]; then
-	rm -rf ux-exp
-	git clone https://github.com/ashwinstr/ux-exp.git
-	cd ux-exp
-	git pull
-	git pull https://github.com/ashwinstr/UX-itachi.git
-	repo="ux-exp"
-fi
-
-if [[ "${u_n}" == "i-u" ]]; then
+if [[ "${u_n}" == "i" ]]; then
 	rm -rf UX-itachi
 	git clone https://github.com/ashwinstr/UX-itachi.git
 	cd UX-itachi
@@ -54,9 +33,9 @@ echo "Updating your ${repo} repo..."
 echo " "
 
 git add .
-git diff
 git commit -m "Windows/termux merge"
 git push
+git diff
 echo " "
 echo "Updated"
 echo " "
