54987c8158c67701dc79e30689491c408b099765
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-05 13:38:17 +0530

    Update creds.sh

--- creds.sh
+++ creds.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-username = "ashwinstr" # your username of github account
-repo = "UX-jutsu" # your fork or cloned repo's name
-upstream = "https://github.com/code-rgb/USERGE-X.git" # the upstream repo link from which the commits will be pulled
+username="ashwinstr" # your username of github account
+repo="UX-jutsu" # your fork or cloned repo's name
+upstream="https://github.com/code-rgb/USERGE-X.git" # the upstream repo link from which the commits will be pulled
 
 echo "###"
 echo "Username - ${username}"
